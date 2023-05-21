4378c700002836e2ab8beefe84de3ea75a3bb6e4
Camila Cristina <camila.alves@nectarcrm.com.br>
2023-05-21 11:12:52 -0300

    O próximo nível

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: '192.168.5.248',
+        port: '3333',
+        pathname: '/uploads/**',
+      },
+    ],
     domains: ['avatars.githubusercontent.com'],
   },
 }
